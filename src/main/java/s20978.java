ad7f8b2af48fec2ffdf2ad21a62079cf20aaaaf5
Grzegorz00 <g.swiecickii@gmail.com>
2020-12-08 16:28:47 +0100

    Initial commit

---
+++ src/main/java/s20978.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class s20978 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(args[0]));
+        Tree tree = new Tree();
+
+        while (sc.hasNext()){
+            tree.add(sc.nextLine());
+        }
+    }
+}
+
+class Node {
+    private char value;
+    private Node left;
+    private Node right;
+
+    public Node() {
+    }
+
+    public Node(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public Node getChild(char whichChild){
+        return whichChild == 'L'? getLeft() : getRight();
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public void setChild(char whichChild, Node child){
+        if (whichChild == 'L') setLeft(child);
+        else setRight(child);
+    }
+}
+
+class Tree {
+    private Node root;
+
+    public Tree() {
+        this.root = new Node();
+    }
+
+    public void add(String line) {
+        char value = line.charAt(0);
+
+        // check if it's not the root
+        if (line.length() <= 2){
+            root.setValue(value);
+            return;
+        }
+
+        Node tmpNodePointer = root;
+        nodeCreator(value,line.substring(2),tmpNodePointer);
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    private void nodeCreator(char value, String children, Node tmpNodePointer){
+        char child =  children.charAt(0);
+
+        if (tmpNodePointer.getChild(child) == null) tmpNodePointer.setChild(child,new Node());
+
+        if (children.length() > 1) nodeCreator(value,children.substring(1),tmpNodePointer.getChild(child));
+        else if(children.length() == 1)tmpNodePointer.setChild(child,new Node(value));
+    }
+
+}
+
+
---
+++ src/test/java/TreeTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TreeTest{
+
+    private Tree tree1 = new Tree();
+
+    @Test
+    public void addTest() {
+        Tree tree = new Tree();
+        tree.add("A");
+        assertEquals("Only root value", 'A', tree.getRoot().getValue());
+
+        //1
+        tree1.add("A RR");
+        assertEquals(
+                "Creating no-value root and right->right node - A",
+                'A',
+                tree1.getRoot().getRight().getRight().getValue()
+        );
+        //2
+        tree1.add("B RL");
+        assertEquals(
+                "Going through existing root->rightNode and creating new leftNode - B",
+                'B',
+                tree1.getRoot().getRight().getLeft().getValue()
+        );
+        //3
+        tree1.add("C R");
+        assertEquals(
+                "Going through existing root and adding value to rightNode - C",
+                'C',
+                tree1.getRoot().getRight().getValue()
+        );
+        //4
+        tree1.add("E RLRL");
+        assertEquals(
+                "Going through existing root->right->left->right and adding value to leftNode - E",
+                'E',
+                tree1.getRoot().getRight().getLeft().getRight().getLeft().getValue()
+        );
+        //4
+        tree1.add("D RLR");
+        assertEquals(
+                "Going through existing root->right->left and adding value to rightNode - D",
+                'D',
+                tree1.getRoot().getRight().getLeft().getRight().getValue()
+        );
+    }
+}
